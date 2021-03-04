31f6ce2a9da0a4693313fc3d9199c908cdfd52d6
Upamita <80004824+Upamita@users.noreply.github.com>
2021-03-04 11:05:22 +0530

    Created productofnumbers.cpp

---
+++ .gitignore/productofnumbers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double r, s, prod;
+    cout << "Enter two numbers: ";
+    cin >> r >> s;
+    prod = r * s;  
+    cout << "Product = " << prod;   
+    return 0;
+}
